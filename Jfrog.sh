4d5ac44378e4734ee2b09403639aa12735ae0d90
root <root@ip-172-31-95-213.ec2.internal>
2024-10-15 13:23:22 +0000

    changes in Jfrog

--- Jfrog.sh
+++ Jfrog.sh
@@ -1,6 +1,6 @@
 ##Install in Amazon Ubuntu
 sudo usermod -aG docker $USER
 docker pull docker.bintray.io/jfrog/artifactory-oss:7.55.10
-sudo mkdir -p /jfrog/artifactory
+sudo mkdir -p ./jfrog/artifactory
 sudo chown -R 1030 /jfrog/
-docker run --name artifactory -d -p 8081:8081  -v /jfrog/artifactory:/var/opt/jfrog/artifactory docker.bintray.io/jfrog/artifactory-oss:𝟳.𝟱𝟱.𝟭𝟬
+docker run -d --name artifactory1  -p 8081:8081 -p 8082:8082  -v /jfrog/artifactory:/var/opt/jfrog/artifactory  docker.bintray.io/jfrog/artifactory-oss:7.55.10
